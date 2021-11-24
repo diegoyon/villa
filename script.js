28ccb295583b1ba54e3a04876aaaaa3241eb6527
Diego Yon <diegoyon@outlook.com>
2021-11-24 17:21:15 -0600

    Added the random function that returns a random number between two numbers

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("yey");
+let z = aleatorio(5,0);
+document.write(z);
+
+function aleatorio(min, max){
+  let resultado;
+  resultado =  Math.floor(Math.random() * (max - min + 1)) + min;
+  return resultado;
+}
