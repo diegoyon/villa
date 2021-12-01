67942ead20739e1121e3550c086e362baaf78c83
Diego Yon <diegoyon@outlook.com>
2021-12-01 10:07:26 -0600

    se puso la vaca en un lugar random

--- script.js
+++ script.js
@@ -1,5 +1,59 @@
-let z = aleatorio(5,0);
-document.write(z);
+let vp = document.getElementById("villa");
+let papel = vp.getContext("2d");
+let cantidad = aleatorio(5,10);
+
+let fondo = {
+  url: "tile.png",
+  cargaOK: false
+}
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+let vaca = {
+  url: "vaca.png",
+  cargaOK: false
+};
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+
+function dibujar(){
+  if(fondo.cargaOK){
+    papel.drawImage(fondo.imagen, 0, 0);
+  }
+  for(let i=0; i<cantidad; i++){
+    if(vaca.cargaOK){
+      let x = aleatorio(0,420);
+      let y = aleatorio(0,420);
+      console.log(x);
+      console.log(y);
+      papel.drawImage(vaca.imagen, x, y);
+    }
+  }
+  
+}
+
+function cargarFondo(){
+  fondo.cargaOK = true;
+  dibujar();
+}
+
+function cargarVacas(){
+  vaca.cargaOK = true;
+}
+
+function cargarPollos(){
+  papel.drawImage(pollo, 20, 20)
+}
+
+function cargarCerdos(){
+  papel.drawImage(cerdo, 50, 50)
+}
+
 
 function aleatorio(min, max){
   let resultado;
